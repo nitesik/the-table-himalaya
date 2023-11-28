97cfcc2ab1e22d2a43b27c3ea80fab38dfea9dad
mactope-github <prashunnaral8@gmail.com>
2023-11-29 01:25:24 +0530

    'fixxx'

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -2,6 +2,14 @@ import logo from '@/assets/table-himalaya-custom.svg';
 import chicken_korai from "@/assets/chicken-korai.jpg"
 import chicken_tikka from "@/assets/chicken-tikka.jpg"
 import mixed_grill from "@/assets/mixed-grill.jpg";
+
+import lamb-thali from "@/assets/lamb-thali.jpg";
+
+
+
+
+
+
 import phone from "@/assets/phone.svg";
 import mail from "@/assets/mail.svg";
 import grape from "@/assets/grape.svg";
@@ -21,6 +29,7 @@ export const icons = {
   chicken_korai,
   chicken_tikka,
   mixed_grill,
+  lamb-thali,
   phone,
   mail,
   grape,
