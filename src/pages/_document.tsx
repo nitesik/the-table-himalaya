a7808b85938a2753c51089e9d5842b7a1f04be6a
nitesik <neeteshchhetri69@gmail.com>
2024-03-19 11:56:41 +0600

    fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4421463422364704"
+        crossOrigin="anonymous"
+      ></Script>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
